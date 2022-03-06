d4521a31b7e3dcef4b41f56f7b8328200fdffa9e
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-07 01:31:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const blog = {
+    id: 1,
+    title: "Blog Title",
+    description: "Blog Description"
+  }
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
