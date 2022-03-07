de22d18c4df2ee4216c3f37ad2ff94d6cfcbfe16
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-08 01:37:36 +0300

    ejs installed, file structure updated

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const ejs = require('ejs')
+const path = require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs') //template engine views klasörü içerisine bakar
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog Description"
-  }
-  res.send(photo);
+  res.render('index')
 });
 
+app.get('/about', (req,res)=>{
+  res.render('about')
+})
+app.get('/addpost', (req,res)=>{
+  res.render('addPost')
+})
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
