5c54d4f23aa41ea173ea014681fcb9a7d06298f3
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-11 04:52:08 +0300

    env file added

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const methodOverride = require('method-override');
+const env = require('dotenv').config()
 const postController = require('./controllers/postController');
 const pageController = require('./controllers/pageController');
 
 const app = express();
 
+
 //Connect DB
-mongoose.connect('mongodb://localhost:27017/cleanblog-test-db', {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bbp6.mongodb.net/cleanblog-db?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(()=>{
+  console.log('DB CONNECTED')
+}).catch(err=>{
+  console.log(err);
 });
 
 //TEMPLATE ENGINE
@@ -41,7 +47,7 @@ app.get('/about', pageController.getAboutPage);
 app.get('/addpost', pageController.getAddPage);
 app.get('/posts/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
